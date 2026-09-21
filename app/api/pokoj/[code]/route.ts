398a1a8b50a21ecaed2ab70f028b3cd8cf651983
damianmro-design <damian.mro@wp.pl>
2026-09-22 02:13:39 +0300

    Pass verified PartyPlay token to secure account join

--- app/api/pokoj/[code]/route.ts
+++ app/api/pokoj/[code]/route.ts
@@ -124,7 +124,7 @@ export async function POST(request: Request, context: RouteContext) {
           code,
           name,
           avatar,
-          partyPlayUser.id,
+          partyPlayAccessToken,
         );
       } else {
         player = await joinPlatformRoom(code, name, avatar);
