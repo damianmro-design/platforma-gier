791cf484241ac8f0139100440ebb5db7078e139a
damianmro-design <damian.mro@wp.pl>
2026-09-22 01:06:21 +0300

    Link Akta Nocy teaser from game catalog

--- app/page.tsx
+++ app/page.tsx
@@ -445,6 +445,7 @@ export default function Home() {
               tags={["murder mystery", "fabularna", "dedukcja"]}
               accent="red"
               art="crime"
+              href="/gry/akta-nocy"
               status="soon"
             />
 
