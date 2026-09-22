72dcf0a339ce532bf7d868ef6fb858f12225084c
damianmro-design <damian.mro@wp.pl>
2026-09-22 23:38:58 +0300

    Use sharp Akta Nocy cover artwork

--- app/gra/akta-nocy/[code]/game-client.tsx
+++ app/gra/akta-nocy/[code]/game-client.tsx
@@ -455,7 +455,7 @@ function HostView({
             </p>
 
             <StageArtwork
-              src="/akta-nocy/hero-v3.webp"
+              src="/akta-nocy/hero-v4.webp"
               alt="Akta Nocy, sprawa Apartament 214"
               cinematic
             />
@@ -1329,7 +1329,7 @@ function PlayerView({
             </p>
 
             <StageArtwork
-              src="/akta-nocy/case-cover-v3.webp"
+              src="/akta-nocy/case-cover-v4.webp"
               alt="Okładka akt sprawy Apartament 214"
             />
 
